160c29dbd8d9da4712812411bf75dfb0f90bc5dc
Benjamin Stein <ben@liatrio.com>
2017-06-15 12:00:24 -0700

    workspace directory needs to exist for files to be copied to it. if it doesn't already exists, the dsl job will create it before copying files to it

--- createDevJob.groovy
+++ createDevJob.groovy
@@ -1,3 +1,11 @@
+def shellCommand = '''
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir /var/jenkins_home/jobs/example/workspace/
+fi
+cp -r ../pipeline-dev/* /var/jenkins_home/jobs/example/workspace/
+'''
+
+
 job("local-pipeline-manager") {
     customWorkspace("/pipeline-dev")
     steps {
@@ -5,7 +13,7 @@ job("local-pipeline-manager") {
             text(readFileFromWorkspace('devJobDefinition.groovy'))
             removeAction('DELETE')
         }
-        shell('cp -r ../pipeline-dev/* /var/jenkins_home/jobs/example/workspace/')
+        shell(shellCommand)
     }
     publishers {
         downstream('devPipeline', 'STABLE')
